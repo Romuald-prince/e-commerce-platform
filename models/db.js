f19c7d5152420a6ce3b5f692842b019769a99bef
romuald abega <romualdabega@gmail.com>
2020-12-22 00:54:00 +0800

    database connection

--- models/db.js
+++ models/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose");
 
-mongoose.connect("mongodb://localhost:27017/ecommerceOrders",{useNewUrlParser:true, useUnifiedTopology: true}, function(err)
+mongoose.connect("mongodb+srv://adminP:test123@cluster0.trqxw.mongodb.net/ecommerceOrders",{useNewUrlParser:true, useUnifiedTopology: true}, function(err)
 {
     if(!err) {
 console.log("MongoDB connected");
